d72ea83250f0c08a6036feceb1b69090a23c78cc
nicoramo2s <nicolasramo2s@hotmail.com>
2023-10-26 11:00:00 -0300

    created schemas users/models

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+import { UserInterface } from "../interfaces/UserInterfaces";
+import { boolean } from "joi";
+
+/* CREACION DE LOS CAMPOS DE LA TABLA USER */
+const userSchema = new Schema<UserInterface>(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmation: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = model("User", userSchema);
+
+export default userModel;
